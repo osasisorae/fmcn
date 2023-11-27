1079b83e104d7dd989772c9c8e55e4667028d218
osasisorae <osasisorae@gmail.com>
2023-11-27 14:15:58 +0100

    refactor: Improve naming and loading state handling in HomePage component
    - Rename 'index' component to 'HomePage' for better clarity.
    - Enhance the logic to handle the loading state when using the 'useSession' hook.
    - Provide appropriate feedback to the user during the loading state.
    - Update button text and styling for login and logout actions.

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,33 +1,35 @@
-import React from 'react'
-import { useSession,signIn,signOut } from 'next-auth/react'
-function index() {
-  const {data:session,status}=useSession()
-  console.log(session)
-  if(status=="authenticated"){
-    return (
-      <>
-   <button
-       type="button"
-       onClick={() => signOut()}
-       className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-       Logout
-     </button>
-     
-      </>
-     )
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+
+function HomePage() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    // Render a loading state if the session is still loading
+    return <p>Loading...</p>;
   }
+
   return (
     <>
- <button
-     type="button"
-     onClick={() => signIn()}
-     className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-     Login
-   </button>
+      {status === 'authenticated' ? (
+        <button
+          type="button"
+          onClick={() => signOut()}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          Logout
+        </button>
+      ) : (
+        <button
+          type="button"
+          onClick={() => signIn()}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+        >
+          Login
+        </button>
+      )}
     </>
-   )
-
- 
+  );
 }
 
-export default index
+export default HomePage;
